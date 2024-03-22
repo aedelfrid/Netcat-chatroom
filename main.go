a9412fb90ebd0427f0cb9866cc4cb0589fa75cd8
aedelfrid <almck51@gmail.com>
2024-03-21 22:55:32 -0300

    made a roadmap and began writing server code

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-
+	"fmt"
+	"net"
 )
 
 func main() {
+	const (
+		network string = "TCP"
+		port    string = "8080"
+	)
+
+	packet, err := net.Listen(network, port)
+
+	if (err != nil) {
+		fmt.Println(err)
+	}
+
 	
 }
